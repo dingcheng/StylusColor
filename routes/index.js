15af112583e41433a6696c7cb8863a30a16ace2c
dingcheng <topren1@gmail.com>
2012-09-28 15:49:18 -0500

    stylus variables support

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,17 @@
  * GET home page.
  */
 var fs = require('fs');
+var variables = {};
+
+function extrvar(str){
+	if (str.match(/=/)){
+		var toks = str.split('=');
+		if (toks.length<2) {return false};
+		variables[toks[0]]=toks[1];
+		return true;
+	}
+	return false;
+}
 
 function indentlevel(line){
 	return line.split(/[^ \t\r\n]/)[0].length;
@@ -22,27 +33,41 @@ exports.proc=function(req,res){
 	var stack=[];
 	var prevlevel=0,curlevel=0,prevline='';
 	var colorset={},curstyle;
+	// For each file in the uploads
 	for(var key in req.files.stylus){
+		//Extract file meta info
 		if (!req.files.stylus[key]) {break};
 		var styl = req.files.stylus[key];
-		if (!styl.filename.match(/.styl/)) {continue};
-		fcon = fs.readFileSync(styl.path,'utf8');
+		if (!styl.filename.match(/.styl/)) {continue};//accept only .styl
+		fcon = fs.readFileSync(styl.path,'utf8');//read file
 		var lines = fcon.split('\n');
-		var prevline = lines[0];
+		//var prevline = lines[0].replace(/[\s]/g,'').split('//')[0];
+		// For each line in the file
 		for(var linenum in lines){
 			line = lines[linenum];
-			if (indentlevel(line)>curlevel) {
+			var level = indentlevel(line);
+			line = line.replace(/[\s]/g,'').split('//')[0];
+			// Detect variable definition, if yes, continue
+			if (extrvar(line))
+				continue;
+			// If it goes into deeper level, push previous line as parent
+			if (level>curlevel)
 				stack.push(prevline);
-			}
-			else if(indentlevel(line)<curlevel){
-				for (var i = 0; i < curlevel-indentlevel(line); i++) {
+			else if(level<curlevel){
+				// If exiting out of level(s), pop parents out
+				for (var i = 0; i < curlevel-level; i++)
 					stack.pop();
-				}
 				curstyle='';
 			}
-			curlevel=indentlevel(line);
+			curlevel=level;
+			// If 'color' is matched in the string, extract this line
 			if (line.match(/color/)) {
 				var selector = stack.join('>');
+				var toks = line.split(':');
+				if (toks[1] in variables)
+					line = toks[0]+': '+variables[toks[1]];
+				else
+					line = toks.join(': ');
 				if (!(selector in colors)){
 					colors[selector]={};
 					colors[selector].place=[];
@@ -55,7 +80,7 @@ exports.proc=function(req,res){
 				}
 				else
 					colors[selector].c+='F';
-				colors[selector].style+=line.replace(/^\s\s*/, '').split('//')[0]+';';
+				colors[selector].style+=line+';';
 				curstyle=colors[selector].style;
 				if (curstyle in colorset) {colors[selector].repeat=true};
 				colorset[curstyle]=1;
